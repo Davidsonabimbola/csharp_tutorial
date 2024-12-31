d3254e0a1a21ae2ee43f0654a8154c7f11874e25
Abimbola Davidson <davidsonmm9@gmail.com>
2024-12-31 22:00:21 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+
+// Console.WriteLine("Hello, Abimbola!");
+
+
+
+// using System;
+// class using Greet;
+// using Grpc.Net.Client;
+using System;
+// using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             Console.WriteLine("Hello, Abimbola!");
+             Main1();
+             decimal d1 = 344.34M;
+             Console.WriteLine(d1)
+             
+        }
+
+        public static void Main1()
+        {
+            Console.WriteLine("Hello,Semilore");
+        }
+    }
+}
